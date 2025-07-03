624564526606cad418d54d72ae332f7e9f36a6de
rahul gaonkar <rahulgaonkar40@gmail.com>
2025-07-03 19:01:57 +0000

    Add missing sub function

--- src/math_operations.py
+++ src/math_operations.py
@@ -1,7 +1,5 @@
-def add (a,b):
-    """Returns the sum of a and b."""
+def add(a, b):
     return a + b
 
-def subtract(a, b):
-    """Returns the difference of a and b."""
+def sub(a, b):
     return a - b
